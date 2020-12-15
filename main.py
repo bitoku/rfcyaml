0442297e6543089e5d10c77aca346caabc05f83a
Ayato Tokubi <tokubi.ayato@gmail.com>
2020-12-15 20:49:06 +0900

    yamlを修正

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import codecs
 import json
 import pathlib
 import re
-from typing import List
+from typing import List, Optional, Tuple, Union
 
 from tqdm import tqdm
 
@@ -116,14 +116,59 @@ def get_sections_l1(text:List[str]) -> List[RFCSection]:
     return [RFCSection(title='__initial_text__', contents=[''.join(text)])]
 
 
-def create_tree(sections: List[RFCSection]):
+def create_tree(sections: List[RFCSection]) -> List[RFCSection]:
+    new_sections: List[RFCSection] = []
+    stack: List[RFCSection] = []
+    latest_section_number = [0]
+    latest_section: Optional[RFCSection] = None
+    temp_sections: List[RFCSection] = []
     for section in sections:
         m = re.match(r'(\d+(\.\d+)*)\.?', section['title'])
-        print(section['title'])
+        if not m:
+            temp_sections.append(section)
+            continue
+        # 番号付きセクションである場合
+        current_section_number: List[int] = list(map(lambda x: int(x), m.group(1).split('.')))
+        if not is_successive_section(latest_section_number, current_section_number):
+            # 連番でない場合
+            raise Exception('section is not successive')
+        if temp_sections:
+            # 番号なしセクションがその前にある場合
+            # 直前のセクションにcontentsを含める
+            if latest_section is None:
+                new_sections += temp_sections
+            else:
+                for temp_section in temp_sections:
+                    latest_section['contents'] += temp_section['contents']
+            temp_sections = []
+        while len(stack) >= len(current_section_number):
+            sec = stack.pop()
+            if len(stack) == 0:
+                new_sections.append(sec)
+            else:
+                stack[-1]['contents'].append(sec)
+        stack.append(section)
+        latest_section_number = current_section_number
+        latest_section = section
+    if temp_sections:
+        new_sections += temp_sections
+    return new_sections
 
 
-def main(x):
-    for i in range(1000, x+1):
+def is_successive_section(latest, current) -> bool:
+    if len(latest) == 0 and len(current) == 0:
+        return True
+    latest_top = latest[0] if len(latest) > 0 else -1
+    current_top = current[0] if len(current) > 0 else -1
+    if latest_top > current_top:
+        return False
+    if latest_top < current_top:
+        return True
+    return is_successive_section(latest[1:], current[1:])
+
+
+def main(start, end):
+    for i in range(start, end+1):
         try:
             rfc = RFC(i)
             if rfc.info['status'] in {
@@ -135,7 +180,8 @@ def main(x):
                 # rfc = RFC(i, get_sections_l0(text))
                 # rfc.dump()
                 print(i)
-                create_tree(rfc.sections)
+                for section in rfc.sections: print(section['title'])
+                print_sec(create_tree(rfc.sections))
                 print()
         except FileNotFoundError:
             continue
@@ -195,6 +241,13 @@ def remove_footer(x: int):
     r.dump()
 
 
+def print_sec(sections: List[Union[str, RFCSection]], indent=0):
+    for section in sections:
+        if isinstance(section, str):
+            continue
+        print('  ' * indent + section['title'])
+        print_sec(section['contents'], indent+1)
+
 if __name__ == '__main__':
     # N = 9000
     # start = 5600
@@ -202,13 +255,10 @@ if __name__ == '__main__':
     #     with Pool(4) as p:
     #         for _ in p.imap_unordered(lines, range(start, N)):
     #             t.update(1)
-    x = 1001
-    # text = preprocess_old(x)
-    # rfc = RFC(x, get_sections_l0(text))
-    # rfc.dump()
-    concat_contents(
-        x,
-        '15.6.  ADAPTER STATUS TRANSACTIONS',
-        '16.  NetBIOS SESSION SERVICE',
-    )
-    main(x)
+    x = 4000
+    # concat_contents(
+    #     x,
+    #     '4.3.1.  IEEE 802.1Q',
+    #     '5.  Intellectual Property'
+    # )
+    main(3000, x)
