aea7c795e54f0917a8707a3a8c4c596fee3e483b
Ayato Tokubi <tokubi.ayato@gmail.com>
2020-12-15 16:26:19 +0900

    yamlを修正

--- main.py
+++ main.py
@@ -69,38 +69,31 @@ def preprocess(n: int) -> List[str]:
     return text
 
 
+def preprocess_old(n: int) -> List[str]:
+    txt_file = RFC_DIR / f'rfc{n}.txt'
+    pages: List[List[str]] = []
+    page: List[str] = []
+    with codecs.open(txt_file, 'r', 'utf-8', 'ignore') as f:
+        for line in f:
+            sep = False
+            for c in line:
+                if ord(c) == 12:
+                    sep = True
+                    break
+            if sep:  # feedforward character
+                pages.append(page)
+                page = []
+                continue
+            page.append(line)
+        if page:
+            pages.append(page)
+    pages = [page[3:-3] for page in pages]
+    pages[0] = ignore_trivial_line(pages[0])
+    text = [line for each_page in pages for line in each_page if line.strip() != '']
+    return text
+
+
 def extract_sections(rfc_n: int, text: List[str]):
-    # These RFCs aren't able to be extracted with this method
-    k0 = [
-        3, 6, 10, 13, 16, 18, 19, 23, 24, 25, 27, 28, 29, 30, 32, 34, 35, 40, 41, 43, 45, 47, 49, 50, 53, 56, 57, 58,
-        59, 63, 64, 66, 67, 68, 70, 71, 73, 75, 77, 81, 85, 86, 87, 93, 96, 98, 99, 103, 104, 105, 106, 107, 111, 113,
-        116, 117, 123, 124, 125, 127, 129, 131, 132, 134, 140, 142, 143, 146, 147, 148, 149, 150, 151, 152, 154, 155,
-        156, 163, 167, 170, 172, 173, 176, 179, 180, 181, 185, 187, 191, 193, 194, 196, 197, 198, 200, 202, 204, 206,
-        207, 210, 212, 214, 216, 218, 224, 225, 226, 227, 228, 230, 231, 232, 234, 235, 236, 240, 242, 251, 253, 254,
-        263, 265, 266, 269, 271, 274, 280, 283, 285, 287, 293, 294, 297, 302, 303, 307, 308, 321, 322, 324, 326, 329,
-        330, 331, 332, 334, 335, 339, 340, 342, 345, 346, 347, 348, 349, 354, 356, 359, 361, 362, 363, 365, 366, 367,
-        370, 371, 372, 373, 376, 382, 384, 386, 387, 390, 391, 394, 395, 396, 398, 399, 400, 401, 402, 405, 406, 410,
-        411, 412, 413, 416, 421, 422, 423, 425, 429, 431, 436, 440, 446, 447, 448, 449, 450, 451, 453, 455, 456, 457,
-        459, 461, 462, 464, 470, 471, 472, 474, 476, 480, 483, 485, 486, 487, 488, 489, 490, 493, 495, 496, 497, 498,
-        500, 504, 506, 508, 511, 512, 513, 521, 523, 526, 531, 533, 544, 548, 550, 551, 552, 556, 559, 567, 568, 569,
-        570, 578, 580, 591, 601, 602, 604, 606, 607, 608, 609, 610, 611, 612, 614, 615, 616, 617, 618, 619, 620, 621,
-        622, 623, 624, 625, 626, 627, 628, 630, 631, 632, 634, 635, 636, 637, 643, 644, 657, 660, 662, 663, 669, 672,
-        674, 677, 683, 684, 685, 687, 689, 690, 692, 695, 697, 698, 699, 700, 701, 702, 703, 704, 705, 706, 707, 708,
-        713, 716, 717, 718, 720, 722, 725, 728, 729, 730, 733, 734, 737, 738, 742, 744, 745, 747, 751, 752, 756, 757,
-        763, 766, 768, 769, 774, 775, 778, 781, 782, 783, 784, 789, 794, 799, 800, 802, 803, 813, 814, 815, 816, 817,
-        818, 825, 826, 828, 829, 830, 832, 833, 834, 835, 836, 837, 838, 839, 842, 843, 844, 845, 846, 847, 848, 849,
-        850, 855, 862, 863, 864, 865, 866, 867, 868, 869, 877, 886, 887, 889, 891, 892, 896, 904, 911, 945, 962,
-    ]
-    k1 = [
-        1005, 1020, 1034, 1035, 1045, 1062, 1083, 1099, 1100, 1117, 1119, 1124, 1128, 1129, 1130, 1131, 1142, 1153,
-        1194, 1196, 1199, 1228, 1245, 1246, 1247, 1250, 1275, 1276, 1277, 1278, 1279, 1280, 1288, 1292, 1296, 1299,
-        1305, 1311, 1319, 1320, 1321, 1340, 1360, 1399, 1410, 1499, 1500, 1540, 1543, 1599, 1600, 1610, 1632, 1699,
-        1700, 1720, 1751, 1760, 1780, 1799, 1800, 1837, 1873, 1878, 1880, 1891, 1899, 1913, 1915, 1920, 1936, 1938,
-        1999,
-    ]
-    l1 = set(k0 + k1)
-    if rfc_n in l1:
-        return get_sections_l1(text)
     return get_sections_l0(text)
 
 
@@ -126,14 +119,11 @@ def get_sections_l1(text:List[str]) -> List[RFCSection]:
 def create_tree(sections: List[RFCSection]):
     for section in sections:
         m = re.match(r'(\d+(\.\d+)*)\.?', section['title'])
-        if not m:
-            print(section['title'])
+        print(section['title'])
 
 
-def main():
-    bad = []
-    ratios = []
-    for i in range(1, 1000):
+def main(x):
+    for i in range(1000, x+1):
         try:
             rfc = RFC(i)
             if rfc.info['status'] in {
@@ -141,8 +131,11 @@ def main():
                 RFCStatus.INTERNET_STANDARD,
                 RFCStatus.DRAFT_STANDARD
             } and not rfc.info['obsoleted_by']:
-                print(rfc.n)
-                remove_footer(rfc.n)
+                # text = preprocess_old(i)
+                # rfc = RFC(i, get_sections_l0(text))
+                # rfc.dump()
+                print(i)
+                create_tree(rfc.sections)
                 print()
         except FileNotFoundError:
             continue
@@ -174,18 +167,18 @@ def lines(n):
         print(n)
 
 
-def concat_contents(x: int):
+def concat_contents(x: int, start: str, end: str):
     rfc = RFC(x)
     sections = []
     stop = False
     for section in rfc.sections:
-        if section['title'] == '7.  IANA Considerations':
+        if section['title'] == end:
             stop = False
         if not stop:
             sections.append(section)
         else:
             sections[-1]['contents'] += section['contents']
-        if section['title'] == '6.2.  Ed448 Examples':
+        if section['title'] == start:
             stop = True
     r = RFC(x, sections)
     r.dump()
@@ -195,8 +188,7 @@ def remove_footer(x: int):
     rfc = RFC(x)
     sections = []
     for section in rfc.sections:
-        if re.match(f'RFC {x}     ', section['title']):
-            print(section['title'])
+        if re.match(f'RFC {x}', section['title']):
             continue
         sections.append(section)
     r = RFC(x, sections)
@@ -210,4 +202,10 @@ if __name__ == '__main__':
     #     with Pool(4) as p:
     #         for _ in p.imap_unordered(lines, range(start, N)):
     #             t.update(1)
-    main()
+    # x = 791
+    # concat_contents(
+    #     x,
+    #     '__initial_text__',
+    #     '1.1.  Motivation',
+    # )
+    main(x)
